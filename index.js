6a1e52bd4108d548ccc5078c5db530f41299f678
Reza <rzgholizadeh@gmail.com>
2020-03-27 21:56:48 -0600

    feat: Parse the data from the form

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
         <div>
             <form method="POST">
                 <input name="email" placeholder="email"/>
-                <input name="password "placeholder="password"/>
+                <input name="password" placeholder="password"/>
                 <input name="passwordConfirmation" placeholder="password confirmation"/>
                 <button>Sign Up</button>
             </form>
@@ -15,8 +15,17 @@ app.get("/", (req, res) => {
     `);
 });
 
-app.post("/", (req,res)=>{
-    res.send("Account created.");
+app.post("/", (req, res) => {
+	req.on("data", data => {
+		const parsed = data.toString("utf8").split("&");
+		const formData = {};
+		for (let pair of parsed) {
+			const [key, value] = pair.split("=");
+			formData[key] = value;
+		}
+		console.log(formData);
+	});
+	res.send("Account created.");
 });
 
 app.listen(7000, () => {
