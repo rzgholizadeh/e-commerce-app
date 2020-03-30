6076474d49d2815587458e078cacc3cbc30ae94c
Reza <rzgholizadeh@gmail.com>
2020-03-30 17:35:07 -0600

    feat: Add sign out

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users");
 
 const app = express();
 
 // applying middleware globally
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	cookieSession({
+		keys: ["someRandomCharacters"] // the encryption key
+	})
+);
 
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
 	res.send(`
         <div>
+            ID: ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email"/>
                 <input name="password" placeholder="password"/>
@@ -20,7 +27,7 @@ app.get("/", (req, res) => {
     `);
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
 	const { email, password, passwordConfirmation } = req.body;
 	const existingUser = await usersRepo.getOneBy({ email });
 	if (existingUser) {
@@ -30,10 +37,31 @@ app.post("/", async (req, res) => {
 		return res.send("Passwrods must match");
 	}
 
-    const user = await usersRepo.create({ email, password });
-    
+	const user = await usersRepo.create({ email, password });
+	req.session.userId = user.id;
 
 	res.send("Account created.");
+});
+
+app.get("/signout", (req, res) => {
+	req.session = null;
+	res.send("logged out");
+});
+
+app.get("/signin", (req, res) => {
+	res.send(`  
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email"/>
+            <input name="password" placeholder="password"/>
+            <button>Sign In</button>
+        </form>
+    </div>
+`);
+});
+
+app.post("/signin", async (req, res) => {
+    
 });
 
 app.listen(7000, () => {
