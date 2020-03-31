4d5ce38c299b9af5fd92d10cbd55f9442f7a2883
Reza <rzgholizadeh@gmail.com>
2020-03-30 19:01:28 -0600

    refactor: extract html generation code

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const usersRepo = require("../../repositories/users");
+const signupTemplate = require("../../views/admin/auth/signup");
+const signinTemplate = require("../../views/admin/auth/signin");
 
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
-	res.send(`
-        <div>
-            ID: ${req.session.userId}
-            <form method="POST">
-                <input name="email" placeholder="email"/>
-                <input name="password" placeholder="password"/>
-                <input name="passwordConfirmation" placeholder="password confirmation"/>
-                <button>Sign Up</button>
-            </form>
-        </div>
-    `);
+	res.send(signupTemplate({ req }));
 });
 
 router.post("/signup", async (req, res) => {
@@ -39,15 +31,7 @@ router.get("/signout", (req, res) => {
 });
 
 router.get("/signin", (req, res) => {
-	res.send(`  
-    <div>
-        <form method="POST">
-            <input name="email" placeholder="email"/>
-            <input name="password" placeholder="password"/>
-            <button>Sign In</button>
-        </form>
-    </div>
-`);
+	res.send(signinTemplate());
 });
 
 router.post("/signin", async (req, res) => {
