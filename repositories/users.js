25cc05551c7927d7c2fc3fd6f9c53068ceb6953a
Reza <rzgholizadeh@gmail.com>
2020-03-30 18:37:15 -0600

    feat: Add password encryption

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
 	constructor(filename) {
@@ -24,10 +27,22 @@ class UsersRepository {
 
 	async create(attrs) {
 		attrs.id = this.randomId();
+		const salt = crypto.randomBytes(8).toString("hex");
+		const buf = await scrypt(attrs.password, salt, 64);
 		const records = await this.getAll();
-		records.push(attrs);
+		const record = {
+			...attrs,
+			password: `${buf.toString("hex")}.${salt}`
+		};
+		records.push(record);
 		await this.writeAll(records);
-		return attrs;
+		return record;
+	}
+
+	async comparePasswords(saved, supplied) {
+		const [hashed, salt] = saved.split(".");
+		const hashedSupplied = await scrypt(supplied, salt, 64);
+		return hashed === hashedSupplied.toString("hex");
 	}
 
 	async writeAll(records) {
