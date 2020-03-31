8b7107eb0781fe6cb4f139c6b321ffc7c3c03843
Reza <rzgholizadeh@gmail.com>
2020-03-30 20:02:13 -0600

    feat: Add custom validations to express validator

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -17,30 +17,34 @@ router.post(
 		check("email")
 			.trim()
 			.normalizeEmail()
-			.isEmail(),
+			.isEmail()
+			.withMessage("Must be a valid email")
+			.custom(async email => {
+				const existingUser = await usersRepo.getOneBy({ email });
+				if (existingUser) {
+					throw new Error("Email is already in use");
+				}
+			}),
 		check("password")
 			.trim()
-			.isLength({ min: 4, max: 20 }),
+			.isLength({ min: 4, max: 20 })
+			.withMessage("Must be between 4 and 20 characters"),
 		check("passwordConfirmation")
 			.trim()
 			.isLength({ min: 4, max: 20 })
+			.withMessage("Must be between 4 and 20 characters")
+			.custom((passwordConfirmation, { req }) => {
+				if (passwordConfirmation !== req.body.password) {
+					throw new Error("Passwords must match");
+				}
+			})
 	],
 	async (req, res) => {
 		const errors = validationResult(req);
 		console.log(errors);
-
 		const { email, password, passwordConfirmation } = req.body;
-		const existingUser = await usersRepo.getOneBy({ email });
-		if (existingUser) {
-			return res.send("Email already in use");
-		}
-		if (password !== passwordConfirmation) {
-			return res.send("Passwrods must match");
-		}
-
 		const user = await usersRepo.create({ email, password });
 		req.session.userId = user.id;
-
 		res.send("Account created.");
 	}
 );
