bab1222347b6f507a34dc7dd6302f24841b2365a
Reza <rzgholizadeh@gmail.com>
2020-03-30 22:10:03 -0600

    feat: Add validation and sanitization for products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
+const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
 
@@ -8,6 +11,14 @@ router.get("/admin/products", (req, res) => {});
 
 router.get("/admin/products/new", (req, res) => {
 	res.send(productsNewTemplate({}));
+});
+
+router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		console.log(errors);
+	}
+	res.send("submitted");
 });
 
 module.exports = router;
