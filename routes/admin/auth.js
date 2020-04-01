098868dc1024c8ba0c1a25d2f5498957f3644e72
Reza <rzgholizadeh@gmail.com>
2020-03-31 22:32:36 -0600

    refactor: Extract validation result code to midlwr

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -21,12 +21,8 @@ router.get("/signup", (req, res) => {
 router.post(
 	"/signup",
 	[requireEmail, requirePassword, requirePasswordConfirmation],
+	handleErrors(signupTemplate),
 	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			console.log(errors);
-			return res.send(signupTemplate({ req, errors }));
-		}
 		const { email, password } = req.body;
 		const user = await usersRepo.create({ email, password });
 		req.session.userId = user.id;
@@ -46,11 +42,8 @@ router.get("/signin", (req, res) => {
 router.post(
 	"/signin",
 	[requireEmailExists, requireValidPasswordForUser],
+	handleErrors(signinTemplate),
 	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.send(signinTemplate({ errors }));
-		}
 		const { email } = req.body;
 		const user = await usersRepo.getOneBy({ email });
 		req.session.userId = user.id;
