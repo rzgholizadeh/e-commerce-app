d898f1d9d4c383cd55ec5a115647f5d3718f2fe4
Reza <rzgholizadeh@gmail.com>
2020-03-31 21:40:28 -0600

    feat: Setup multer library

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
+const multer = require("multer");
 
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -13,16 +15,19 @@ router.get("/admin/products/new", (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		console.log(errors);
-	}
-	req.on("data", data => {
-		console.log(data.toString());
-	});
+router.post(
+	"/admin/products/new",
+	[requireTitle, requirePrice],
+	upload.single("image"),
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			console.log(errors);
+		}
+		console.log(req.file);
 
-	res.send("submitted");
-});
+		res.send("submitted");
+	}
+);
 
 module.exports = router;
