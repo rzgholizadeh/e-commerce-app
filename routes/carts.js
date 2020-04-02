719dc8c2eada49da6a680fbb12fe1c5f265d86d9
Reza <rzgholizadeh@gmail.com>
2020-04-01 19:53:25 -0600

    feat: Add the cart show template to the views

--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
 const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
